9e31e0003e1adec243302c85ce55dce325b8c601
MuchinaWamburia <lucymuchina20@gmail.com>
2023-06-19 12:42:08 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,22 +7,16 @@
  */
 int main(void)
 {
-int num1, num2;
-for (num1 = 0; num1 < 10; num1++)
+int num;
+for (num = 0; num < 10; num++)
 {
-for (num2 = num1; num2 < 10; num2++)
-{
-putchar(num1 + '0');
-putchar(num2 + '0');
-
-if (num1 != 9 || num2 != 9)
+putchar(num + '0');
+if (num != 9)
 {
 putchar(',');
 putchar(' ');
 }
 }
-}
 putchar('\n');
-
 return (0);
 }
