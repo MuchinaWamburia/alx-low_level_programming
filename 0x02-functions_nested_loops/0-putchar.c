bd1682ea9968c0dd6763b6f668a0a9931f5a9e17
MuchinaWamburia <lucymuchina20@gmail.com>
2023-06-20 12:02:19 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point of the program
+ * Description: Prints "_putchar" followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char *str = "_putchar\n";
+int i = 0;
+while (str[i] != '\0')
+{
+write(1, &str[i], 1);
+i++;
+}
+return (0);
+}
