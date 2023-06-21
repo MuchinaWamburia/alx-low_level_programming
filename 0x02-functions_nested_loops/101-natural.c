933a69a143dff82f8356e8046605eeb7d50ed9a5
MuchinaWamburia <lucymuchina20@gmail.com>
2023-06-21 16:47:30 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - Entry point of the program
+*
+* Return: Always 0
+*/
+int main(void)
+{
+int limit = 1024;
+int sum = 0;
+int i;
+
+for (i = 1; i < limit; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+{
+sum += i;
+}
+}
+
+printf("The sum of all the multiples of 3 or 5 below 1024 is: %d\n", sum);
+
+return (0);
+}
