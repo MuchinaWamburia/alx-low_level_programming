40acfddb3e154a0dbb90e496a0a8ad098a57bca6
MuchinaWamburia <lucymuchina20@gmail.com>
2023-06-21 17:38:29 -0700

    corrected

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,32 +8,20 @@ void print_to_98(int n)
 {
 if (n <= 98)
 {
-while (n <= 98)
+while (n < 98)
 {
-_putchar((n / 10) + '0');
-_putchar((n % 10) + '0');
-if (n != 98)
-{
-_putchar(',');
-_putchar(' ');
-}
+printf("%d, ", n);
 n++;
 }
 }
 else
 {
-while (n >= 98)
+while (n > 98)
 {
-_putchar((n / 10) + '0');
-_putchar((n % 10) + '0');
-
-if (n != 98)
-{
-_putchar(',');
-_putchar(' ');
-}
+printf("%d, ", n);
 n--;
 }
 }
-_putchar('\n');
+
+printf("%d\n", n);
 }
