78a76fa1ead53f6e2386ccb42d46844e0a3745b8
MuchinaWamburia <lucymuchina20@gmail.com>
2023-06-21 17:55:24 -0700

    corrected

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,30 +1,12 @@
 #include "main.h"
 
 /**
-* print_number - prints a number
-* @num: the number to be printed
-*/
-void print_number(int num)
-{
-if (num < 10)
-{
-_putchar(num + '0');
-}
-else
-{
-print_number(num / 10);
-_putchar((num % 10) + '0');
-}
-}
-
-/**
 * print_times_table - prints the n times table, starting with 0
 * @n: number of the times table
 */
 void print_times_table(int n)
 {
 int row, column, product;
-
 if (n >= 0 && n <= 15)
 {
 for (row = 0; row <= n; row++)
@@ -32,30 +14,35 @@ for (row = 0; row <= n; row++)
 for (column = 0; column <= n; column++)
 {
 product = column * row;
-
-if (column > 0)
+if (column == 0)
+{
+_putchar(product + '0');
+}
+else if (product < 10 && column != 0)
 {
 _putchar(',');
 _putchar(' ');
-}
-
-if (product <= 9)
-{
 _putchar(' ');
 _putchar(' ');
-print_number(product);
+_putchar(product + '0');
 }
 else if (product >= 10 && product < 100)
 {
+_putchar(',');
 _putchar(' ');
-print_number(product);
+_putchar(' ');
+_putchar((product / 10) + '0');
+_putchar((product % 10) + '0');
 }
 else if (product >= 100)
 {
-print_number(product);
+_putchar(',');
+_putchar(' ');
+_putchar((product / 100) + '0');
+_putchar(((product / 10) % 10) + '0');
+_putchar((product % 10) + '0');
 }
 }
 _putchar('\n');
-}
 }
 }
