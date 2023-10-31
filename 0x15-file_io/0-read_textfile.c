7643fb5d37f50c1ba37788a5b23d97a621f3e86b
MuchinaWamburia <lucymuchina20@gmail.com>
2023-10-31 22:44:09 +0300

    updated

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,8 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include "main.h"
+#include <stdlib.h>
 
+/**
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
+ *
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
     int fd;
@@ -12,12 +19,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
     if (filename == NULL)
         return (0);
 
-    // Open the file for reading
+    /* Open the file for reading */
     fd = open(filename, O_RDONLY);
     if (fd == -1)
         return (0);
 
-    // Allocate memory for a buffer
+    /* Allocate memory for a buffer */
     buffer = malloc(letters);
     if (buffer == NULL)
     {
@@ -25,7 +32,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
         return (0);
     }
 
-    // Read from the file
+    /* Read from the file */
     bytes_read = read(fd, buffer, letters);
     if (bytes_read == -1)
     {
@@ -34,10 +41,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
         return (0);
     }
 
-    // Write to standard output
+    /* Write to standard output */
     bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 
-    // Clean up and close the file
+    /* Clean up and close the file */
     close(fd);
     free(buffer);
 
